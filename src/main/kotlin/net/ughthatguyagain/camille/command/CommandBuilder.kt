b2e6a9922661b58840aef12aeb1a46385d569d05
Jarrett <jarrettklink1@gmail.com>
2018-07-25 01:37:16 -0400

    addition of the summoner command

--- src/main/kotlin/net/ughthatguyagain/camille/command/CommandBuilder.kt
+++ src/main/kotlin/net/ughthatguyagain/camille/command/CommandBuilder.kt
@@ -2,18 +2,23 @@ package net.ughthatguyagain.camille.command
 
 import com.merakianalytics.orianna.Orianna
 import com.merakianalytics.orianna.types.common.Region
+import net.ughthatguyagain.camille.Config
 
 import net.ughthatguyagain.camille.command.commands.Test
 import net.ughthatguyagain.camille.command.commands.ArgsTest
+import net.ughthatguyagain.camille.command.commands.Summoner
 import net.ughthatguyagain.camille.command.commands.TestApi
 
 class CommandBuilder(val commands: HashMap<String, ICommand<String>> =  HashMap()) {
     init {
-        Orianna.setRiotAPIKey("RGAPI-8adb433a-ae4e-4a59-9a4d-e51f29ec135e")
-        Orianna.setDefaultRegion(Region.NORTH_AMERICA)
+        val config = Config()
+        Orianna.setRiotAPIKey(config.key)
         commands["test"] = Test()
         commands["argstest"] = ArgsTest()
         commands["testapi"] = TestApi()
+        commands["summoner"] = Summoner()
+
+        println("Commands loaded.")
     }
 
 }
