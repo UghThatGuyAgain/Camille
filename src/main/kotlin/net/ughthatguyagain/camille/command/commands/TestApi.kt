6b2c8a7cd171322984239aec6c8a23560a02b95d
Jarrett <jarrettklink1@gmail.com>
2018-07-25 01:37:51 -0400

    quickfix for TestApi with region configuration

--- src/main/kotlin/net/ughthatguyagain/camille/command/commands/TestApi.kt
+++ src/main/kotlin/net/ughthatguyagain/camille/command/commands/TestApi.kt
@@ -1,6 +1,7 @@
 package net.ughthatguyagain.camille.command.commands
 
 import com.merakianalytics.orianna.Orianna
+import com.merakianalytics.orianna.types.common.Region
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent
 import net.ughthatguyagain.camille.command.ICommand
 import net.ughthatguyagain.camille.command.MessageBuilder
@@ -10,6 +11,7 @@ class TestApi: ICommand<String> {
     override val arguments = true
     override val description = "lol xd"
     override fun execute(args: List<String>, event: MessageReceivedEvent) {
+        Orianna.setDefaultRegion(Region.NORTH_AMERICA)
         val summoner = Orianna.summonerNamed(args[0]).get()
         val mB = MessageBuilder()
         mB.mB.setTitle("lol").setDescription(summoner.name)
